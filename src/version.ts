b0056298d69626027ec1dcbf0f7a57f53d7a3892
Andrew Gscheidle <gscheidleinc@gmail.com>
2019-06-03 16:57:00 -0400

    Feature | sbmvp-277 install PostGrotesk font (#20)
    * Install font
    
    SBMVP-277
    
    * Fixing bad stash merge
    
    * SBMVP-277 - Providing non-broken font

--- src/version.ts
+++ src/version.ts
@@ -24,12 +24,12 @@ export interface AppVersion {
 
 export const VERSION: AppVersion = {
     dirty: true,
-    raw: "403de22-dirty",
-    hash: "403de22",
+    raw: "72bd807-dirty",
+    hash: "72bd807",
     distance: null,
     tag: null,
     semver: null,
-    suffix: "403de22-dirty",
+    suffix: "72bd807-dirty",
     semverString: null,
-    version: "0.0.67"
+    version: "0.0.70"
 };
