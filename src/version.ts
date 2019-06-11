98d4bce10b1492f6e60bb24ced1daf0a98822937
revelfire <chrismathiaster@gmail.com>
2019-06-11 07:43:40 -0500

    Adding auto-delete-merged-branches

--- src/version.ts
+++ src/version.ts
@@ -1,35 +1,36 @@
 /* IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT! */
 
 export interface SemVer {
-    raw: string;
-    major: number;
-    minor: number;
-    patch: number;
-    prerelease: any[];
-    build: any[];
-    version: string;
+  raw: string;
+  major: number;
+  minor: number;
+  patch: number;
+  prerelease: any[];
+  build: any[];
+  version: string;
 }
 
 export interface AppVersion {
-    dirty: boolean;
-    raw: string;
-    hash: string;
-    distance: number;
-    tag: string;
-    semver: SemVer;
-    suffix: string;
-    semverString: string;
-    version: string;
+  dirty: boolean;
+  raw: string;
+  hash: string;
+  distance: number;
+  tag: string;
+  semver: SemVer;
+  suffix: string;
+  semverString: string;
+  version: string;
 }
 
 export const VERSION: AppVersion = {
-    dirty: true,
-    raw: "40d43b1-dirty",
-    hash: "40d43b1",
-    distance: null,
-    tag: null,
-    semver: null,
-    suffix: "40d43b1-dirty",
-    semverString: null,
-    version: "0.0.87"
+    "dirty": true,
+    "raw": "2fdaeb7-dirty",
+    "hash": "2fdaeb7",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "2fdaeb7-dirty",
+    "semverString": null,
+    "version": "0.0.88"
 };
+
