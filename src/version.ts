dc30c3356e3ea402617291257fdf6e7c47f655ec
revelfire <chrismathiaster@gmail.com>
2019-06-18 12:27:20 -0700

    Removing security bits

--- src/version.ts
+++ src/version.ts
@@ -1,35 +1,36 @@
 /* IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT! */
 
 export interface SemVer {
-    raw: string;
-    major: number;
-    minor: number;
-    patch: number;
-    prerelease: any[];
-    build: any[];
-    version: string;
+  raw: string;
+  major: number;
+  minor: number;
+  patch: number;
+  prerelease: any[];
+  build: any[];
+  version: string;
 }
 
 export interface AppVersion {
-    dirty: boolean;
-    raw: string;
-    hash: string;
-    distance: number;
-    tag: string;
-    semver: SemVer;
-    suffix: string;
-    semverString: string;
-    version: string;
+  dirty: boolean;
+  raw: string;
+  hash: string;
+  distance: number;
+  tag: string;
+  semver: SemVer;
+  suffix: string;
+  semverString: string;
+  version: string;
 }
 
 export const VERSION: AppVersion = {
-    dirty: true,
-    raw: "698f1e5-dirty",
-    hash: "698f1e5",
-    distance: null,
-    tag: null,
-    semver: null,
-    suffix: "698f1e5-dirty",
-    semverString: null,
-    version: "0.0.127"
+    "dirty": true,
+    "raw": "15475d1-dirty",
+    "hash": "15475d1",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "15475d1-dirty",
+    "semverString": null,
+    "version": "0.0.128"
 };
+
