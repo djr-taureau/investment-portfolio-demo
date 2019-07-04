65bf292e8dde42c22a9fd3102f43c708b0d9e4f0
Chris Mathias <chrismathiaster@gmail.com>
2019-07-04 11:58:27 -0700

    SBMVP-428 attempting build artifact for pipeline (#63)

--- zip-deploy.sh
+++ zip-deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-version = $(cat package.json | jq -r '.version')
+app_version=$(cat package.json | jq -r '.version')
+git_hash=$(git log --pretty=format:'%h' -n 1)
+version="$app_version.$git_hash"
+echo "Creating zip for version $version"
 zip -r prism-spa-$version.zip ./dist
