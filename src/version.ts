00be3013d73597aeec87ef582b6b086ce49c0975
Chris Mathias <chrismathiaster@gmail.com>
2019-08-06 11:05:22 -0500

    Sbmvp 598 (#111)
    * Swagger change: give Company a Contact for teamLead
    
    * Swagger change: removing defunct Revenue definitions
    
    * Minor change to shore up group by for portco list
    
    * Shoring up for portco info page model
    
    * SBMVP-865 - KPI endpoint, and some cleanup of incorrect response definitions given latest design decisions
    
    * Portfolio/Fund relationship and API
    
    * SBMVP-598 Documents

--- src/version.ts
+++ src/version.ts
@@ -24,12 +24,12 @@ export interface AppVersion {
 
 export const VERSION: AppVersion = {
     dirty: true,
-    raw: "6ed8fcd-dirty",
-    hash: "6ed8fcd",
+    raw: "f756f0b-dirty",
+    hash: "f756f0b",
     distance: null,
     tag: null,
     semver: null,
-    suffix: "6ed8fcd-dirty",
+    suffix: "f756f0b-dirty",
     semverString: null,
-    version: "0.0.257"
+    version: "0.0.258"
 };
